5d78cb285f52d89f8e053470955dacf4343e0647
imodoiepale <imodoiepale@gmail.com>
2022-10-14 01:08:29 +0300

    footer

--- google-react-clone/components/Footer.js
+++ google-react-clone/components/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
       <div className="  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 grid-flow-row-dense">
         <div className="flex justify-center items-center md:col-span-2 lg:col-span-1 lg:col-start-2 ">
           <GlobeAltIcon className="h-5 mr-1 text-green-700" /> Carbon Neutral
-          since 01 B.C
+          since James Epale
         </div>
         <div className="flex justify-center  space-x-8 whitespace-nowrap md:justify-self-start">
           <p> Advertising</p>
